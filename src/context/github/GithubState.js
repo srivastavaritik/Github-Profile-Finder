f83bdc98d3056107b9ac5ca9c007b4535e924cb4
srivastavaritik <srivastavaritik360@gmail.com>
2021-08-30 16:51:50 +0530

    Update GithubState.js

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -2,3 +2,10 @@ import React, {useReducer} from 'react';
 import axios from 'axios';
 import GithubContext from './githubContext';
 import GithubReducer from './githubReducer';
+import { 
+SEARCH_USERS,
+SET_LOADING,
+CLEAR_USERS,
+GET_USER,
+GET_REPOS
+} from '../types';
