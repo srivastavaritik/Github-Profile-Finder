074367d9b9a41e62fd2ae90a219ae4a4e570249f
srivastavaritik <srivastavaritik360@gmail.com>
2021-08-31 01:26:50 +0530

    done!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import Navbar from './components/layouts/Navbar';
 import Home from './components/pages/Home';
 import Alert from './components/layouts/Alert';
@@ -21,6 +22,7 @@ const App = () => {
               <Route exact path="/" component={Home}/>
               <Route exact path="/about" component={About} />
               <Route exact path="/user/:login" component={User} />
+              <Route component={NotFound} />
             </Switch>
           </div>
         </Router>
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,6 +10,18 @@ import {
   GET_REPOS,
 } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production'){
+    githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+}
+else{
+    githubClientId = process.env.GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = (props) => {
   const initialState = {
     users: [],
@@ -25,7 +37,7 @@ const GithubState = (props) => {
     console.log(text);
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&CLIENT_SECRET=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&CLIENT_SECRET=${githubClientSecret}`
     );
     dispatch({
       type: SEARCH_USERS,
@@ -37,7 +49,7 @@ const GithubState = (props) => {
   const getUser = async (username) => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&CLIENT_SECRET=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${githubClientId}&CLIENT_SECRET=${githubClientSecret}`
     );
     dispatch({
         type: GET_USER,
@@ -49,7 +61,7 @@ const GithubState = (props) => {
   const getUserRepos = async (username) => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&CLIENT_SECRET=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}/repos?client_id=${githubClientId}&CLIENT_SECRET=${githubClientSecret}`
     );
     dispatch({
         type: GET_REPOS,
