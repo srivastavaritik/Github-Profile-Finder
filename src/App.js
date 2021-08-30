648e33638f6388a0626ede8fa9176a5fe53c1ccc
srivastavaritik <srivastavaritik360@gmail.com>
2021-08-31 01:13:00 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { Fragment} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import About from './components/pages/About';
 import Navbar from './components/layouts/Navbar';
+import Home from './components/pages/Home';
 import Alert from './components/layouts/Alert';
-import Search from './components/users/Search';
-import Users from './components/users/Users';
 import User from './components/users/User';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 
 const App = () => {
- return (
+  return (
     <GithubState>
       <AlertState>
         <Router>
@@ -19,18 +18,7 @@ const App = () => {
             <Navbar />
             <Alert />
             <Switch>
-              <Route
-                exact
-                path="/"
-                render={(props) => (
-                  <Fragment>
-                    <Search  />
-                    <div className="container">
-                      <Users />
-                    </div>
-                  </Fragment>
-                )}
-              ></Route>
+              <Route exact path="/" component={Home}/>
               <Route exact path="/about" component={About} />
               <Route exact path="/user/:login" component={User} />
             </Switch>
