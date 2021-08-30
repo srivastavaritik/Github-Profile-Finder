dd264cdfaebe3b87362f19aa1953fd7b3252fa4d
srivastavaritik <srivastavaritik360@gmail.com>
2021-08-30 16:00:20 +0530

    Update User.js

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -8,7 +8,7 @@ const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
     useEffect(()=>{
         getUser(match.params.login);
         getUserRepos(match.params.login);
-    });
+    },[]);
 
     const {
       name,
